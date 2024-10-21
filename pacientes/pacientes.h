5cc4d2e3705e997bae80d391fe390398d4c0d898
jsalustianoo <josesalustianojr@gmail.com>
2024-10-21 01:00:26 -0300

    Modularizando Módulo Pacientes

---
+++ pacientes/pacientes.h
@@ -0,0 +1,10 @@
+#ifndef PACIENTES_H
+#define PACIENTES_H
+
+void menu_paciente (void);
+void cadastro_paciente (void);
+void exibir_dados_paciente (void);
+void editar_paciente (void);
+void excluir_paciente (void);
+
+#endif
